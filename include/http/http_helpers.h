381e46633b23d9a8c018e5d254ffa0baad1979c4
lukaskochbusiness <lukas.koch.business@protonmail.com>
2026-02-22 22:35:05 +0700

    fix: de/eng über beide Webseiten verbunden; Sonderzeichen in Städtenamen gehen jetzt (Ü)

--- include/http/http_helpers.h
+++ include/http/http_helpers.h
@@ -5,5 +5,6 @@
 #include "esp_http_server.h"
 
 bool http_read_body(httpd_req_t *req, char *buf, size_t buf_len, size_t *out_len);
+bool http_url_decode(const char *in, char *out, size_t out_len);
 void http_send_json(httpd_req_t *req, int status_code, const char *json);
 void http_send_err(httpd_req_t *req, int status_code, const char *msg);
